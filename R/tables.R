a2db3e07aa1cea7c6ddc41eec3d26f0e13275732
Michal Majka <michalmajka@hotmail.com>
2019-04-08 00:22:06 +0200

    Preparing for 0.9.6 version: improving print method

--- R/tables.R
+++ R/tables.R
@@ -1,6 +1,12 @@
-tables <- function (object, which = NULL)
-{
-    vars <- names(object$tables)
+
+`[.naive_bayes_tables` <- function(x, i, ...) {
+    class(x)  <- "list"
+    res <- x[i]
+    class(res) <- "naive_bayes_tables"
+    res
+}
+
+print.naive_bayes_tables <- function(object, ...) {
 
     n_char <- getOption("width")
     str_left_right <- paste0(rep("=", floor((n_char - 11)/2)),
@@ -10,7 +16,53 @@ tables <- function (object, which = NULL)
     len <- nchar(str_full)
     l <- paste0(rep("-", len), collapse = "")
     cat("\n")
-    cat(str_full, "\n", "\n")
+    cat(str_full, "\n")
+
+    n <- length(object)
+    for (i in 1:n) {
+        ith_tab <- object[[i]]
+        ith_name <- names(object)[i]
+        if (class(ith_tab) == "array") {
+            for (ith_factor in names(ith_tab)) {
+                if (i != 1) {
+                    cat(l)
+                    cat("\n")
+                }
+                cat("\n")
+                cat(paste0(ith_name, "::", ith_factor, " (KDE)", "\n"))
+                print(ith_tab[[ith_factor]])
+            }
+        } else {
+            if (i != 1) {
+                cat(l)
+                cat("\n")
+            }
+            cat("\n")
+            if (any(rownames(ith_tab) == "lambda") & nrow(ith_tab) == 1) {
+                dist <- "(Poisson)"
+            }
+            if (nrow(ith_tab) == 2 & all(!rownames(ith_tab) %in% c("mean", "sd"))) {
+                dist <- "(Bernoulli)"
+            }
+            if (nrow(ith_tab) > 2) {
+                dist <- "(Multinomial)"
+            }
+            if (nrow(ith_tab) == 2 & all(rownames(ith_tab) %in% c("mean", "sd"))) {
+                dist <- "(Gaussian)"
+            }
+            cat(paste0("Feature: ", ith_name, " ", dist, "\n"))
+            print(ith_tab)
+            cat("\n")
+
+        }
+    }
+    cat(l)
+}
+
+tables <- function (object, which = NULL) {
+
+    vars <- names(object$tables)
+    tables <- object$tables
 
     if (is.character(which) && !all(which %in% vars))
         stop("At least one variable is not available")
@@ -30,6 +82,6 @@ tables <- function (object, which = NULL)
     if (is.character(which))
         v <- vars[vars %in% which]
 
-    object$tables[v]
+    tables[v]
 }
 
