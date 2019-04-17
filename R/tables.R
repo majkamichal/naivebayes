21071c2d49c02da6633df7c51c6e2582a756c396
Michal Majka <michalmajka@hotmail.com>
2019-04-17 19:39:01 +0200

    Minor improvements

--- R/tables.R
+++ R/tables.R
@@ -1,23 +1,15 @@
 tables <- function (object, which = NULL) {
 
-    if (!class(object) %in% models()) {
+    obj_class <- class(object)
+    if (!obj_class %in% models()) {
         stop(paste0("tables(): Tables are available only for ",
                     paste0(models(), collapse = ", "),
                     " objects."), call. = FALSE)
     }
-    if (class(object) == "bernoulli_naive_bayes") {
-        tabs <- get_bernoulli_tables(object$prob1)
-    } else if  (class(object) == "gaussian_naive_bayes") {
-        tabs <- get_gaussian_tables(object$params)
 
-        # Need a general function get_tables: input class, say, "naive_bayes" and
-        # then it applies specialized get_*_tables and returns the desired tables.
-
-    } else {
-        tabs <- object$tables
-    }
+    tabs <- get_tables(object)
+    cond_dist <- get_cond_dist(tabs)
     vars <- names(tabs)
-    cond_dist <- get_cond_dist(object)
 
     if (is.null(cond_dist))
         cond_dist <- recognize_cond_dist(tabs)
@@ -46,9 +38,28 @@ tables <- function (object, which = NULL) {
 }
 
 
+get_tables <- function(object) {
+    model <- class(object)
+    if (!model %in% models()) {
+        stop(paste0("tables(): Tables are available only for ",
+                    paste0(models(), collapse = ", "),
+                    " objects."), call. = FALSE)
+    }
+
+    switch(model,
+           "naive_bayes" = object$tables,
+           "bernoulli_naive_bayes" = get_bernoulli_tables(object$prob1),
+           "gaussian_naive_bayes" = get_gaussian_tables(object$params)
+           # "poisson_naive_bayes" = get_poisson_tables(object$params),
+           # "multinomial_naive_bayes" = get_multinomial_tables(object$probs),
+           # "nonparametric_naive_bayes" = get_nonparametric_tables(object$dens)
+    )
+}
+
 get_bernoulli_tables <- function(prob1) {
     if (!is.matrix(prob1))
         stop("prob1 has to be a matrix and prob1 element of the bernoulli_naive_bayes object")
+    vars <- rownames(prob1)
     n_tables <- nrow(prob1)
     tables <- lapply(seq_len(n_tables), function(i) {
         ith_row <- prob1[i, ]
@@ -56,8 +67,8 @@ get_bernoulli_tables <- function(prob1) {
         rownames(ith_tab) <- c("0", "1")
         ith_tab
     })
-    names(tables) <- rownames(prob1)
-    attr(tables, "cond_dist") <- rep("Bernoulli", n_tables)
+    names(tables) <- vars
+    attr(tables, "cond_dist") <- stats::setNames(rep("Bernoulli", n_tables), vars)
     class(tables) <- "naive_bayes_tables"
     tables
 }
@@ -65,6 +76,7 @@ get_bernoulli_tables <- function(prob1) {
 get_gaussian_tables <- function(params) {
     if (!is.list(params))
         stop("get_gaussian_tables(): params has to be a list with parameter estimates.", call. = FALSE)
+    vars <- colnames(mu)
     mu <- params$mu
     sd <- params$sd
     n_tables <- ncol(mu)
@@ -75,8 +87,8 @@ get_gaussian_tables <- function(params) {
         rownames(ith_tab) <- c("mu", "sd")
         ith_tab
     })
-    names(tables) <- colnames(mu)
-    attr(tables, "cond_dist") <- rep("Gaussian", n_tables)
+    names(tables) <- vars
+    attr(tables, "cond_dist") <- stats::setNames(rep("Gaussian", n_tables), vars)
     class(tables) <- "naive_bayes_tables"
     tables
 }
